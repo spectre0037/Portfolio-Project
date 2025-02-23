2f685434c3adc867d91f4c230eb4df11e0c1571a
Abdullah-crns <u2023037@giki.edu.pk>
2025-02-23 12:28:00 +0500

    updated

--- js/Elements.js
+++ js/Elements.js
@@ -1,7 +1,7 @@
 const ElementString = { aboutCard: "about-card", resumeCard: "resume-card", worksCard: "works-card", blogsCard: "blogs-card", contactCard: "contact-card", menuInputs: "menu" }, Element = { ".about-card": document.querySelector(`.${ElementString.aboutCard}`), ".resume-card": document.querySelector(`.${ElementString.resumeCard}`), ".works-card": document.querySelector(`.${ElementString.worksCard}`), ".blogs-card": document.querySelector(`.${ElementString.blogsCard}`), ".contact-card": document.querySelector(`.${ElementString.contactCard}`) };
 document.getElementById("downloadBtn").addEventListener("click", function () {
     const link = document.createElement("a");
-    link.href = "../../GANESH's Resume_DataScience.pdf";  // Replace this with the actual path to your CV file
+    link.href = "../GANESH's Resume_DataScience.pdf";  // Replace this with the actual path to your CV file
     link.download = "My_CV.pdf";        // This will be the name of the downloaded file
     link.click();
 });
